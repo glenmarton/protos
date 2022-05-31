966f35f6b530c821ef18c66f35382a2c2242b37a
Glen Johnson <gjohnson@valcom.com>
2022-05-31 13:36:07 -0400

    Make testing of protos.py more complete

--- python/tests/test_protos.py
+++ python/tests/test_protos.py
@@ -1,25 +1,67 @@
 import unittest
 import sys
 
-from logscribe.eprint import eprint
-from logscribe.fileio import read_file
-from logscribe.fileio import read_yaml_file
+from protos.protos import convert_to_prototype
+from protos.protos import is_function_declaration
+from protos.protos import is_complete
+from protos.protos import process_input
 
 class TestProtos(unittest.TestCase):
 
-  def test_read_file(self):
-    fname = './data/template.yml'
-    contents = read_yaml_file(fname)
-    actual = contents.keys()
-    expect = "dict_keys(['issue', 'type', 'description', 'model', 'milestone'])"
-    self.assertEqual(str(actual), str(expect))
+  def test_prototype_conversion(self):
+    expect = 'int doit (int a, int b, int c);'
 
-  def test_read_file_without_file(self):
-    fname = 'not_a_file.conf'
-    with self.assertRaises(FileNotFoundError): read_file(fname)
+    actual = convert_to_prototype('int doit(int a, int b, int c)')
+    self.assertEqual(expect, actual, 'Prototype is not correct for single line.')
 
-  def test_read_conf(self):
-    fname = './data/conftest.conf'
-    actual = read_file(fname)
-    expect = ['YAML_PATH=./yaml_dir', "CHANGELOG='test_path/test_CHANGELOG.md'", 'a']
-    self.assertEqual(str(actual), str(expect))
+  def test_prototype_conversion_spaces_in_parenthesis(self):
+    expect = 'int doit (int a, int b, int c);'
+    actual = convert_to_prototype('int doit( int a, int b, int c )')
+    self.assertEqual(expect, actual, 'Prototype is not correct for single line.')
+
+  def test_prototype_conversion_brace_with_cpp_comment(self):
+    expect = 'int doit (int a, int b, int c);  // comment'
+    actual = convert_to_prototype('int doit (int a, int b, int c) { // comment')
+    self.assertEqual(expect, actual, 'Prototype is not correct for single line.')
+
+  def test_prototype_conversion_with_c_comment(self):
+    expect = 'int doit (int a, int b, int c); /* comment */'
+    actual = convert_to_prototype('int doit (int a, int b, int c) /* comment */')
+    self.assertEqual(expect, actual, 'Prototype is not correct for single line.')
+
+  def test_close_of_single_line_function_declaration(self):
+    actual = is_complete('void doit(int a)');
+    self.assertTrue(actual)
+
+  def test_close_of_function_declaration(self):
+    actual = is_complete('void doit(int a');
+    self.assertFalse(actual)
+
+  def test_close_of_function_declaration_multiline_start(self):
+    actual = is_complete('void doit(int a');
+    self.assertFalse(actual)
+
+  def test_close_of_function_declaration_multiline_end(self):
+    actual = is_complete('	int a)');
+    self.assertTrue(actual)
+
+  def test_close_of_function_declaration_actually_a_call(self):
+    actual = is_complete('	doit (a);');
+    self.assertFalse(actual)
+
+  def test_read_input_singleline(self):
+
+    expect='int process(int a, int b, int c, int d, int e, int f) {'
+    line ='int process(int a, int b, int c, int d, int e, int f) {'
+    actual='unset'
+    actual=process_input(actual, line)
+    self.assertEqual(expect, actual)
+
+  def test_read_input_multiline(self):
+    expect='int doit(int a, int b,\n    int c, int d,\n    int e, int f) {\n'
+    actual='unset'
+    lines = [ 'int doit(int a, int b,\n', '    int c, int d,\n', '    int e, int f) {\n' ]
+    for line in lines:
+      actual = process_input(actual, line)
+
+    self.assertEqual(expect, actual)
