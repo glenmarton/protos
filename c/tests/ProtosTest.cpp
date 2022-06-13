ad159cf756c69a72438e1c23e9d27916d4742045
Glen Johnson <gjohnson@valcom.com>
2022-06-12 21:21:51 -0400

    Add testing of extract_from_line_to_prototype()

--- c/tests/ProtosTest.cpp
+++ c/tests/ProtosTest.cpp
@@ -26,40 +26,84 @@ TEST_GROUP(Protos)
 TEST(Protos, ExtractDeclarationBlankLine)
 {
 	char expect[2] = "";
-	char actual[9] = "Unset";
+	char actual[9] = "";
 
-	extract_from_line_to_declaration((char*)"", actual);
+	extract_from_line_to_prototype((char*)"", actual);
 	STRCMP_EQUAL (expect, actual);
 }
 
 TEST(Protos, ExtractDeclarationGood)
 {
-	char expect[32] = "float add(float a, float b)";
-	char actual[64] = "Unset";
+	char expect[32] = "float add(float a, float b);";
+	char actual[64] = "";
 
-	extract_from_line_to_declaration((char*)"float add(float a, float b)", actual);
+	extract_from_line_to_prototype((char*)"float add(float a, float b)", actual);
+	STRCMP_EQUAL (expect, actual);
+}
+
+TEST(Protos, ExtractDeclarationPartial)
+{
+	char input[32] = "float add(float a,\n";
+	char expect[32] = "float add(float a,\n";
+	char actual[64] = "";
+
+	extract_from_line_to_prototype(input, actual);
+	STRCMP_EQUAL (expect, actual);
+}
+
+TEST(Protos, ExtractDeclarationMultiLine)
+{
+	char input[32] = "\tfloat b)\n";
+	char expect[32] = "float add(float a,\n\tfloat b);";
+	char actual[64] = "float add(float a,\n";
+
+	extract_from_line_to_prototype(input, actual);
+	STRCMP_EQUAL (expect, actual);
+}
+
+TEST(Protos, ExtractDeclarationTrippleLine)
+{
+	char input1[32] = "char* merge(char* a,\n";
+	char input2[32] = "\tchar* b,\n";
+	char input3[32] = "\tchar* c)\n";
+	char expect[64] = "char* merge(char* a,\n\tchar* b,\n\tchar* c);";
+	char actual[64] = "";
+
+	extract_from_line_to_prototype(input1, actual);
+	extract_from_line_to_prototype(input2, actual);
+	extract_from_line_to_prototype(input3, actual);
 	STRCMP_EQUAL (expect, actual);
 }
 
 TEST(Protos, ExtractDeclaration)
 {
 	char input[64] = "float add(float a, float b) { ";
-	char expect[32] = "float add(float a, float b)";
-	char actual[64] = "Unset";
-	extract_from_line_to_declaration((char*)input, actual);
+	char expect[32] = "float add(float a, float b);";
+	char actual[64] = "";
+	extract_from_line_to_prototype((char*)input, actual);
 	STRCMP_EQUAL (expect, actual);
 }
 
 TEST(Protos, ExtractDeclarationWithSpaces)
 {
 	char input[32] = "int inc(int a)   ";
-	char expect[32] = "int inc(int a)";
-	char actual[64] = "Unset";
+	char expect[32] = "int inc(int a);";
+	char actual[64] = "";
 
-	extract_from_line_to_declaration(input, actual);
+	extract_from_line_to_prototype(input, actual);
 	STRCMP_EQUAL (expect, actual);
 }
-/* Tests for extract_from_line_to_declaration.
+
+TEST(Protos, ExtractDeclarationWithClosingBrace)
+{
+	char input[32] = "int inc(int a){";
+	char expect[32] = "int inc(int a);";
+	char actual[64] = "";
+
+	extract_from_line_to_prototype(input, actual);
+	STRCMP_EQUAL (expect, actual);
+}
+/* Tests for extract_from_line_to_prototype.
 float add(float a, float b){ 
 int inc(int a) {
 int dec(int a) 
